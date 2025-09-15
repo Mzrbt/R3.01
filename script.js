a89a5758f1c8d29347dab7d6b6a3c579d93332ee
jabet241 <marin.jabet@etu.unicaen.fr>
2025-09-15 16:45:54 +0200

    Initialisation projet + exo1

---
+++ script.js
@@ -0,0 +1,3 @@
+const tempDegCelsius = 80;
+const celToFahr = tempDegCelsius * 9/5 + 32;
+document.getElementById("text1").textContent = "La température en celsius est de " + tempDegCelsius + " et celle en Fahrenheit est de " + celToFahr; 
