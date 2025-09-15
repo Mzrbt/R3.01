6ddb36e8deb2cefe70046c501bf4cc130efef001
jabet241 <marin.jabet@etu.unicaen.fr>
2025-09-15 17:56:13 +0200

    Création du bouton

---
+++ TD1/Shifumi/script.js
@@ -0,0 +1,9 @@
+const button = document.querySelector("input");
+const paragraph = document.querySelector("p");
+
+button.value = "Cliquez ici"
+button.addEventListener("click", updateButton);
+
+function updateButton() {
+    
+}
