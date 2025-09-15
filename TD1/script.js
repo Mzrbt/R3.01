5d50aa339585ebebae124a80c738800d1b4535d5
jabet241 <marin.jabet@etu.unicaen.fr>
2025-09-15 17:10:54 +0200

    Exo 3 & 4

--- TD1/script.js
+++ TD1/script.js
@@ -8,3 +8,16 @@ const longueur = 8;
 const largeur = 10;
 const aire = largeur * longueur;
 document.getElementById("text2").textContent = "L'aire du rectangle est le produit de la longueur " + longueur + " et de la largeur " + largeur + " ce qui nous donne une aire de " + aire + " m²"
+
+/*Exo 3*/
+const nom = "Langlois"
+const prénom = "Marin"
+document.getElementById("text3").textContent = prénom + " " + nom;
+
+/*Exo 4*/
+const montantHT = 50;
+const tauxTVA = 0.2;
+const montantTTC = montantHT + (montantHT * (1 - tauxTVA));
+document.getElementById("text4").textContent = "Montant HT = " + montantHT + ", taux TVA = " + tauxTVA + " et montant TTC = " + montantTTC
+
+/*Exo 5*/
