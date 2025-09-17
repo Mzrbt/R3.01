38507897bb72effe83f30cf21644dc30c78e91bd
jabet241 <marin.jabet@etu.unicaen.fr>
2025-09-17 09:49:40 +0200

    Fonctionnel

--- TD1/Shifumi/script.js
+++ TD1/Shifumi/script.js
@@ -18,19 +18,27 @@ buttons.forEach(button => {
 
         if (choix_joueur == "Pierre"){
             choix.innerHTML = "Votre choix : Pierre";
-            choix_bot.innerHTML = "Choix du bot : " + bot
+            choix_bot.innerHTML = "Choix du bot : " + bot;
         } else if (choix_joueur == "Feuille"){
             choix.innerHTML = "Votre choix : Feuille";
-            choix_bot.innerHTML = "Choix du bot : " + bot
+            choix_bot.innerHTML = "Choix du bot : " + bot;
         } else {
             choix.innerHTML = "Votre choix : Ciseaux";
-            choix_bot.innerHTML = "Choix du bot : " + bot
+            choix_bot.innerHTML = "Choix du bot : " + bot;
         }
 
-        if ((choix_joueur && bot == "Pierre") || (choix_joueur && bot == "Feuille") || (choix_joueur && bot == "Ciseaux")){
-            res.innerHTML = "Match nul"
-            
+        if (choix_joueur === bot){
+            res.innerHTML = "Match nul";
+            resultats.nulles += 1;
+        } else if ((choix_joueur === "Pierre" && bot === "Ciseaux") || (choix_joueur === "Feuille" && bot === "Pierre") || (choix_joueur === "Ciseaux" && bot === "Feuille")){
+            res.innerHTML = "Vous avez gagné !";
+            resultats.victoires += 1;
+        } else {
+            res.innerHTML = "Vous avez perdu...";
+            resultats.defaites += 1;
         }
+
+        liste_res.innerHTML = "Victoires : " + resultats.victoires + ", Défaites : " + resultats.defaites + ", Nulles : " + resultats.nulles;
 
     });
 });
