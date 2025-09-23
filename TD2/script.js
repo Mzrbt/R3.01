afd419b3cafc149e95430b3d4a87784be2ad0ade
Mzrbt <jabetmarin07@gmail.com>
2025-09-23 11:27:16 +0200

    Traçage du pad et de la balle

--- TD2/script.js
+++ TD2/script.js
@@ -1,5 +1,15 @@
-var c = document.getElementById("myCanvas");
-var ctx = c.getContext("2d");
-ctx.moveTo(0, 0);
-ctx.lineTo(200, 100);
-ctx.stroke();
+const canvas = document.getElementById('pong');
+const ctx = canvas.getContext('2d');
+
+const rectLength = 80;
+const rectWidth = 7.5;
+
+const circRadius = 15
+
+ctx.fillStyle = 'black';
+ctx.fillRect((canvas.width / 2) - (rectLength / 2), canvas.height - 20, rectLength, rectWidth);
+
+ctx.beginPath();
+ctx.arc((canvas.width / 2), canvas.height - 40, circRadius, 0, 2 * Math.PI);
+ctx.fillStyle = 'black';
+ctx.fill();
